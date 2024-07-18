a4c87454c7d1f8e373214eb404a4b61ed517d5fb
HuongPhanBeReady <huong.phan@beready.academy>
2024-07-18 15:47:39 +0700

    change position code page

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,62 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
+/** @var yii\web\View $this */
+/** @var string $content */
+
+use app\assets\AppAsset;
+use app\widgets\Alert;
+use yii\bootstrap5\Breadcrumbs;
+use yii\bootstrap5\Html;
+use yii\bootstrap5\Nav;
+use yii\bootstrap5\NavBar;
+
+AppAsset::register($this);
+
+$this->registerCsrfMetaTags();
+$this->registerMetaTag(['charset' => Yii::$app->charset], 'charset');
+$this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1, shrink-to-fit=no']);
+$this->registerMetaTag(['name' => 'description', 'content' => $this->params['meta_description'] ?? '']);
+$this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_keywords'] ?? '']);
+$this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii::getAlias('@web/favicon.ico')]);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>" class="h-100">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To Do List</title>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
+<body class="d-flex flex-column h-100">
+<?php $this->beginBody() ?>
 
-<body>
-    <div x-data="toDoList()" class="max-w-2xl mx-auto px-12 py-8 rounded-lg shadow-lg bg-gray-300 h-screen flex flex-col">
-        <div class="flex justify-between items-center mb-2">
-            <div class="text-lg font-semibold">
-                To Dos: <span x-text="toDoCount()"></span> | Completed: <span x-text="numberOfToDosCompleted()"></span>
-            </div>
-            <div>
-                <select x-model="filter" class="px-4 py-2 rounded shadow text-lg">
-                    <option value="all">All</option>
-                    <option value="completed">Completed</option>
-                    <option value="notCompleted">Not Completed</option>
-                </select>
-            </div>
-        </div>
-        <div class=" text-lg mb-2">
-            <template x-if="allCompleted()">
-                <span>Congrats you to finished your list!</span>
-            </template>
-            <template x-if="!allCompleted()">
-                <span><span x-text="toDoCount() - numberOfToDosCompleted()"></span> more to go</span>
-            </template>
-        </div>
-        <div class="bg-white w-full rounded shadow mb-2 flex-grow overflow-auto">
-            <template x-for="(todo, index) in filteredTodos()" :key="index">
-                <div class="flex items-center py-4 px-4" :class="{ 'border-b border-gray-400': !isLastToDo(index) }">
-                    <div class="w-1/12 text-center">
-                        <input type="checkbox" @change="toggleToDoCompleted(index)" :checked="todo.completed">
-                    </div>
-                    <div class="w-10/12">
-                        <p x-text="todo.todo" :class="{ 'line-through': todo.completed }"></p>
-                    </div>
-                    <div class="w-1/12 flex items-center justify-center">
-                      <button class="bg-red-600 text-white px-2 py-1 rounded hover:bg-red-700" @click="deleteToDo(index)">
-                          Remove
-                      </button>
-                  </div>
-                  
-                </div>
-            </template>
-        </div>
-        <div class="flex justify-end items-center space-x-4">
-          <input type="text" x-model="newTodo" placeholder="Add to do item" class="flex-grow px-4 py-2 rounded shadow text-lg" @keydown.enter="addToDo">
-          <button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded shadow" @click="addToDo">Add</button>
-      </div>      
-      
+<main id="main" class="flex-shrink-0" role="main">
+    <div class="container">
+        <?php if (!empty($this->params['breadcrumbs'])): ?>
+            <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
+        <?php endif ?>
+        <?= Alert::widget() ?>
+        <?= $content ?>
     </div>
-    <script src="/assets/app.js"></script>
+</main>
 
+<?php $this->endBody() ?>
 </body>
-
 </html>
+<?php $this->endPage() ?>
--- views/site/index.php
+++ views/site/index.php
@@ -2,52 +2,51 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+$this->title = 'My To do list App';
 ?>
-<div class="site-index">
-
-    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+    <div x-data="toDoList()" class="max-w-2xl mx-auto px-12 py-8 rounded-lg shadow-lg bg-gray-300 h-screen flex flex-col">
+        <div class="flex justify-between items-center mb-2">
+            <div class="text-lg font-semibold">
+                To Dos: <span x-text="toDoCount()"></span> | Completed: <span x-text="numberOfToDosCompleted()"></span>
             </div>
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            <div>
+                <select x-model="filter" class="px-4 py-2 rounded shadow text-lg">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="notCompleted">Not Completed</option>
+                </select>
             </div>
         </div>
-
+        <div class=" text-lg mb-2">
+            <template x-if="allCompleted()">
+                <span>Congrats you to finished your list!</span>
+            </template>
+            <template x-if="!allCompleted()">
+                <span><span x-text="toDoCount() - numberOfToDosCompleted()"></span> more to go</span>
+            </template>
+        </div>
+        <div class="bg-white w-full rounded shadow mb-2 flex-grow overflow-auto">
+            <template x-for="(todo, index) in filteredTodos()" :key="index">
+                <div class="flex items-center py-4 px-4" :class="{ 'border-b border-gray-400': !isLastToDo(index) }">
+                    <div class="w-1/12 text-center">
+                        <input type="checkbox" @change="toggleToDoCompleted(index)" :checked="todo.completed">
+                    </div>
+                    <div class="w-10/12">
+                        <p x-text="todo.todo" :class="{ 'line-through': todo.completed }"></p>
+                    </div>
+                    <div class="w-1/12 flex items-center justify-center">
+                      <button class="bg-red-600 text-white px-2 py-1 rounded hover:bg-red-700" @click="deleteToDo(index)">
+                          Remove
+                      </button>
+                  </div>
+                  
+                </div>
+            </template>
+        </div>
+        <div class="flex justify-end items-center space-x-4">
+          <input type="text" x-model="newTodo" placeholder="Add to do item" class="flex-grow px-4 py-2 rounded shadow text-lg" @keydown.enter="addToDo">
+          <button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded shadow" @click="addToDo">Add</button>
+      </div>      
+      
     </div>
-</div>
+    <script src="/assets/app.js"></script>
